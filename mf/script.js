979ccbddbe328ff670431d162a879a0fc69859dc
Gonzalo Vargas Ludwig <gonzalovargasludwig@lawn-143-215-84-188.lawn.gatech.edu>
2018-04-06 18:55:22 -0400

    mifut

--- mf/script.js
+++ mf/script.js
@@ -1,7 +1,37 @@
+function bcol(area){
+  console.log(area);
+  switch (area) {
+    case 'Salud':
+      return '#8dd3c7';
+    case 'Tecnología':
+      return '#ffffb3';
+    case 'Administración y Comercio':
+      return '#bebada';
+    case 'Agropecuaria':
+      return '#fb8072';
+    case 'Arte y Arquitectura':
+      return '#80b1d3';
+    case 'Ciencias Sociales':
+      return '#fdb462'
+    case 'Derecho':
+      return '#b3de69'
+    case 'Educación':
+      return '#fccde5';
+    case 'Ciencias Básicas':
+      return '#d9d9d9';
+    case 'Humanidades':
+      return '#bc80bd';
+    default:
+      return 'white';
+  }
+}
+
+
 var app = new Vue({
       el: '#app',
       data: {
-        info: null
+        info: null,
+        bcol: "red"
       },
 
       created: function () {
@@ -12,7 +42,11 @@ var app = new Vue({
             this.info = JSON.parse(response.bodyText);
 
             console.log(response.body);
-        
+
+            for (let k = 0; k < this.info.length; k++) {
+              this.info[k]["bcolor"] = bcol(this.info[k].area);
+              this.info[k]["id"] = k;
+            }        
           }, response => {
             // error callback
             console.log('error');
@@ -21,8 +55,11 @@ var app = new Vue({
       methods: {
         filterType: function () {
           console.log("button");
-          
+        },
+        
+      },
+      computed: {
+        
         }
-      }
       
     })
