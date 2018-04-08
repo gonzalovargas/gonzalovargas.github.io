cdf7732da9d600e71ffc528c38820fd6e887f49c
Gonzalo Vargas Ludwig <gonzalovargasludwig@lawn-143-215-84-188.lawn.gatech.edu>
2018-04-08 14:01:54 -0400

    11

--- mf/script.js
+++ mf/script.js
@@ -1,4 +1,4 @@
-function bcol(area){
+function bcol(area) {
   switch (area) {
     case 1:
       return '#8dd3c7';
@@ -18,7 +18,7 @@ function bcol(area){
       return '#fccde5';
     case 9:
       return '#d9d9d9';
-    case 10 :
+    case 10:
       return '#bc80bd';
     default:
       return 'white';
@@ -27,49 +27,70 @@ function bcol(area){
 
 
 var app = new Vue({
-      el: '#app',
-      data: {
-        info: null,
-        bcol: "red",
-        shownumber: 50
-      },
+  el: '#app',
+  data: {
+    info: null,
+    bcol: "red",
+    shownumber: 1000,
+    showU: true,
+    showCFT: true,
+    showIP: true,
+    areas: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  },
 
-      created: function () {
-        // `this` points to the vm instance
-        this.$http.get('https://raw.githubusercontent.com/gonzalovargas/gonzalovargas.github.io/master/mf/data.json').then(response => {
+  created: function () {
+    // `this` points to the vm instance
+    this.$http.get('https://raw.githubusercontent.com/gonzalovargas/gonzalovargas.github.io/master/mf/data.json').then(response => {
 
-            // get body data
-            this.info = JSON.parse(response.bodyText);
-            for (let k = 0; k < this.info.length; k++) {
-              this.info[k]["bcolor"] = bcol(this.info[k].area);
-              this.info[k]["id"] = k;
-              this.info[k]["show"] = k<50;
-            }        
-          }, response => {
-            // error callback
-            console.log('error');
-          });
-      },
-      methods: {
-        filterType: function () {
-          console.log("filter");
-          for (let k = 0; k < this.info.length; k++) {
-            if (this.info[k]["tipo"] == 'Universidad'){
-              this.info[k]["show"] = false;
-            }
-          }
-          this.$forceUpdate();
-        },
-        showMore: function(){
-          this.shownumber += 50;
-          for (let k = 0; k < this.shownumber; k++) {
-              this.info[k]["show"] = true;
-            }
-            this.$forceUpdate();
-          } 
-        },
-      computed: {
-        
+      // get body data
+      this.info = JSON.parse(response.bodyText);
+      for (let k = 0; k < this.info.length; k++) {
+        this.info[k]["bcolor"] = bcol(this.info[k].area);
+        this.info[k]["id"] = k;
+        this.info[k]["show"] = k < this.shownumber;
+      }
+    }, response => {
+      // error callback
+      console.log('error');
+    });
+  },
+  methods: {
+    filterType: function () {
+      console.log("filter");
+      for (let k = 0; k < this.info.length; k++) {
+        if (this.info[k]["tipo"] == 'Universidad') {
+          this.info[k]["show"] = false;
         }
-      
-    })
+      }
+      this.$forceUpdate();
+    },
+    showMore: function () {
+      this.shownumber += 72;
+      for (let k = 0; k < this.shownumber; k++) {
+        this.info[k]["show"] = true;
+      }
+      this.$forceUpdate();
+    }
+  },
+  computed: {
+
+  },
+  filters: {
+    addpoints: function (value) {
+      if (!value) return '-------'
+      if (value == '-') return '-------'
+      value = value.toString()
+      if (value.length <= 6){
+        return '$' + value.substring(0, 3) + '.' + value.substring(3, 6);
+      } else {
+        return '$' + value.charAt(0) + '.' + value.substring(0, 3) + '.' + value.substring(3, 6);
+      }
+    },
+    pointtocomma: function (value) {
+      if (!value) return '-------'
+      if (value.toString() == '-') return '-------'
+      return value.toString().replace(".", ",")
+    }
+  }
+
+})
